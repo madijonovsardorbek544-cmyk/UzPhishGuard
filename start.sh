936a27dcfac65d22878328608aae155d18cefc8e
Sardorbek Madijonov <madijonovsardorbek544@gmail.com>
2026-06-11 18:54:22 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# 1. Telegram botni orqa fonda (background) ishga tushiramiz
+# 1. Ma'lumotlar bazasini noldan tekshirish va yaratish uchun bir marta asosiy kodni yurgizamiz
+python -c "import main; main.init_db()"
+
+# 2. Telegram botni mustaqil fonda ishga tushirish
 python main.py &
 
-# 2. Streamlit Dashboardni asosiy jarayon sifatida oldingi planda ishga tushiramiz
+# 3. Streamlit dashboardni asosiy jarayon sifatida portda yoqish
 streamlit run dashboard.py --server.port=8501 --server.address=0.0.0.0
