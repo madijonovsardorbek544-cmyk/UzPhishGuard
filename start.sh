58f31e49c7202e68ea5d550d843b065afed1b95f
Sardorbek Madijonov <madijonovsardorbek544@gmail.com>
2026-06-17 16:51:13 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# 1. Streamlit dashboardni Render porti orqali fonda (&) ishga tushirish
-# Bu Render skanerini aldash va "No open ports detected" xatosini yo'qotish uchun kerak
-streamlit run dashboard.py --server.port $PORT --server.address 0.0.0.0 &
+# dashboard.py faylining haqiqiy joylashgan yo'lini ko'rsatamiz
+streamlit run app/dashboard/dashboard.py --server.port $PORT --server.address 0.0.0.0 &
 
-# 2. Telegram botni asosiy jarayon sifatida ishga tushirish
+# Telegram botni ishga tushirish
 python app/bot/main.py
